daceed7949652a9971688a17592d8f57e0362c77
Malted <malted@malted.dev>
2024-12-03 14:21:45 -0500

    Initial Commit

---
+++ GitGud/GitGudApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitGudApp.swift
+//  GitGud
+//
+//  Created by Ben Dixon on 03/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitGudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitGudTests/GitGudTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitGudTests.swift
+//  GitGudTests
+//
+//  Created by Ben Dixon on 03/12/2024.
+//
+
+import Testing
+@testable import GitGud
+
+struct GitGudTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
